fff7953c18b4a0ff5a397ea1a6301ae8218106de
Lucas Cavalcanti <lucas.cavalcanti.santos@gmail.com>
2018-10-16 18:13:57 -0300

    ahocorasick fixed =) with complete main

--- projeto1/src/main.cpp
+++ projeto1/src/main.cpp
@@ -6,34 +6,34 @@
 #include "ahocorasick.h"
 
 int main (int argc, char* argv[]){
-    // Shift_Or a;
-    // a.set_char_mask("aba");
-    // std::vector<unsigned int> q_s = a.find("abcababa", "aba");
-    // std::cout << "Shift_Or : " << q_s.size() << " - [";
-    // for (unsigned int q : q_s)
-    // {
-    //     std::cout << q << ", ";
-    // }
-    // std::cout << "]" << std::endl;
+    Shift_Or a;
+    a.set_char_mask("aba");
+    std::vector<unsigned int> q_s = a.find("abcababa", "aba");
+    std::cout << "Shift_Or : " << q_s.size() << " - [";
+    for (unsigned int q : q_s)
+    {
+        std::cout << q << ", ";
+    }
+    std::cout << "]" << std::endl;
 
-    // Wu_Manber b;
-    // b.set_char_mask("cada");
-    // std::vector<unsigned int> q_w = b.find("abadac", "cada", 2);
-    // std::cout << "Wu_Manber: " << q_w.size() << " - [";
-    // for (unsigned int q : q_w)
-    // {
-    //     std::cout << q << ", ";
-    // }
-    // std::cout << "]" << std::endl;
+    Wu_Manber b;
+    b.set_char_mask("cada");
+    std::vector<unsigned int> q_w = b.find("abadac", "cada", 2);
+    std::cout << "Wu_Manber: " << q_w.size() << " - [";
+    for (unsigned int q : q_w)
+    {
+        std::cout << q << ", ";
+    }
+    std::cout << "]" << std::endl;
 
-    // Sellers c;
-    // std::vector<unsigned int> q_c = c.find("abadac", "cada", 2);
-    // std::cout << "Sellers: " << q_c.size() << " - [";
-    // for (unsigned int q : q_c)
-    // {
-    //     std::cout << q << ", ";
-    // }
-    // std::cout << "]" << std::endl;
+    Sellers c;
+    std::vector<unsigned int> q_c = c.find("abadac", "cada", 2);
+    std::cout << "Sellers: " << q_c.size() << " - [";
+    for (unsigned int q : q_c)
+    {
+        std::cout << q << ", ";
+    }
+    std::cout << "]" << std::endl;
 
     std::string txt = "she sells sea shells at the sea shore for her friends";
     std::vector<std::string> patts = {"he", "she", "his", "hers"};

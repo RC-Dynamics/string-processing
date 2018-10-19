26989a00d4d64296a5fecfa276faad64179f4aad
Lucas Cavalcanti <lucas.cavalcanti.santos@gmail.com>
2018-10-18 22:01:52 -0300

    ahocorasick reading files

--- projeto1/src/main.cpp
+++ projeto1/src/main.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
 
-#include "shift_or.h"
-#include "wu_manber.h"
-#include "sellers.h"
-#include "ahocorasick.h"
 #include "utils.h"
 
 
@@ -13,72 +9,28 @@ void test();
 int main (int argc, char* argv[]){
     utils::argmnts arg;
     utils::getArguments(&arg, argc, argv);
-    switch (utils::choseAlg(&arg)) {
-        case ahocorasick:
-            printf("\nAHOCORASICK\n");
-            break;
-        case shiftor:
-            printf("\nSHIFTOR\n");
-            break;
-        case wumanber:
-            printf("\nWUMANBER\n");
-            break;
-        case sellers:
-            printf("\nSELLERS\n");
-            break;
-        default:
-            printf("\nInvalid Option Choose\n");
-            break;
+    int ch = utils::choseAlg(&arg);
+    if (ch == ahocorasick){
+        // printf("\nAHOCORASICK\n");
+        Ahocorasick d;
+        d.build_fsm(arg.patterns);
+        int count = utils::run_aho(arg, &d);
+        if(arg.count_only)
+            printf("\nTotal Number of Occurrences: %d\n", count);
     }
+    else if (ch == shiftor){
+        // printf("\nSHIFTOR\n");
+    }
+    else if (ch == wumanber)
+    {
+        // printf("\nWUMANBER\n");
+    }
+    else if (ch == sellers)
+    {
+        // printf("\nSELLERS\n");
+    }
+    else
+        printf("\nInvalid Option Choose\n");
     return 0;
-}
-
-void test()
-{
-    Shift_Or a;
-    a.set_char_mask("aba");
-    std::vector<unsigned int> q_q = a.find("abcababa", "aba");
-    std::cout << "Shift_Or : " << q_q.size() << " - [";
-    for (unsigned int q : q_q)
-    {
-        std::cout << q << ", ";
-    }
-    std::cout << "]" << std::endl;
-
-    Wu_Manber b;
-    b.set_char_mask("cada");
-    std::vector<unsigned int> q_w = b.find("abadac", "cada", 2);
-    std::cout << "Wu_Manber: " << q_w.size() << " - [";
-    for (unsigned int q : q_w)
-    {
-        std::cout << q << ", ";
-    }
-    std::cout << "]" << std::endl;
-
-    Sellers c;
-    std::vector<unsigned int> q_c = c.find("abadac", "cada", 2);
-    std::cout << "Sellers: " << q_c.size() << " - [";
-    for (unsigned int q : q_c)
-    {
-        std::cout << q << ", ";
-    }
-    std::cout << "]" << std::endl;
-
-    std::string txt = "she sells sea shells at the sea shore for her friends";
-    std::vector<std::string> patts = {"he", "she", "his", "hers"};
-    Ahocorasick d;
-    d.build_fsm(patts);
-    std::vector<std::vector<uint_fast16_t>> q_a = d.find(txt, patts);
-    std::cout << "Ahocorasick : " << q_a.size() << " - [";
-    for (std::vector<uint_fast16_t> q : q_a)
-    {
-        std::cout << "[ ";
-        for (uint_fast16_t a : q)
-        {
-            std::cout << a << ", ";
-        }
-        std::cout << "], ";
-    }
-    std::cout << "]" << std::endl;
 }
 
--- projeto1/src/utils.h
+++ projeto1/src/utils.h
@@ -3,7 +3,12 @@
 #include <string>
 #include <fstream>
 
-#define DEBUG_UTILS 1
+#include "shift_or.h"
+#include "wu_manber.h"
+#include "sellers.h"
+#include "ahocorasick.h"
+
+#define DEBUG_UTILS 0
 
 #define AHO "aho-corasick"
 #define SHI "shiftOr"
@@ -166,4 +171,78 @@ namespace utils {
             return wumanber;
         }
     }
+
+    uint run_aho(argmnts arg, Ahocorasick *aho){
+        uint count = 0;
+        uint last_count = 0;
+        for(uint i = 0; i < arg.txt_files.size(); i++){
+            std::ifstream f(arg.txt_files[i]);
+            std::string line;
+            std::vector<std::vector<uint_fast16_t> > occ;
+            while (getline(f, line)){
+                occ = aho->find(line, arg.patterns);
+                for(auto a : occ){
+                    count += a.size();
+                }
+                if (!arg.count_only && last_count != count){
+                    printf("%s\n", line.c_str());
+                    
+                }
+                last_count = count;
+            }
+            
+        }
+        return count;
+    }
 }
+
+#if DEBUG_UTILS
+void test()
+{
+    Shift_Or a;
+    a.set_char_mask("aba");
+    std::vector<unsigned int> q_q = a.find("abcababa", "aba");
+    std::cout << "Shift_Or : " << q_q.size() << " - [";
+    for (unsigned int q : q_q)
+    {
+        std::cout << q << ", ";
+    }
+    std::cout << "]" << std::endl;
+
+    Wu_Manber b;
+    b.set_char_mask("cada");
+    std::vector<unsigned int> q_w = b.find("abadac", "cada", 2);
+    std::cout << "Wu_Manber: " << q_w.size() << " - [";
+    for (unsigned int q : q_w)
+    {
+        std::cout << q << ", ";
+    }
+    std::cout << "]" << std::endl;
+
+    Sellers c;
+    std::vector<unsigned int> q_c = c.find("abadac", "cada", 2);
+    std::cout << "Sellers: " << q_c.size() << " - [";
+    for (unsigned int q : q_c)
+    {
+        std::cout << q << ", ";
+    }
+    std::cout << "]" << std::endl;
+
+    std::string txt = "she sells sea shells at the sea shore for her friends";
+    std::vector<std::string> patts = {"he", "she", "his", "hers"};
+    Ahocorasick d;
+    d.build_fsm(patts);
+    std::vector<std::vector<uint_fast16_t>> q_a = d.find(txt, patts);
+    std::cout << "Ahocorasick : " << q_a.size() << " - [";
+    for (std::vector<uint_fast16_t> q : q_a)
+    {
+        std::cout << "[ ";
+        for (uint_fast16_t a : q)
+        {
+            std::cout << a << ", ";
+        }
+        std::cout << "], ";
+    }
+    std::cout << "]" << std::endl;
+}
+#endif

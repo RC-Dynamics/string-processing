effd2d9a30c667951032bacacaf165104b9dd914
Roberto <betocostaf@gmail.com>
2018-12-02 21:40:18 -0300

    Adding some values to test lz77

--- ipmt/src/main.cpp
+++ ipmt/src/main.cpp
@@ -4,6 +4,13 @@
 int main () {
     LZ77 lz(7,4);
     std::string str = "cbacbacbcacbacbcaac";
+    // lz.set_buffer_size(512, 128);
+    // lz.set_buffer_size(511, 127);
+    // lz.set_buffer_size(255, 127);
+    // lz.set_buffer_size(256, 128);
+    // lz.set_buffer_size(65535, 255);
+    // lz.set_buffer_size(65535, 511);
+    // lz.set_buffer_size(65535, 4095);
     // std::cout << "Encoding: " << str << std::endl;
 
     std::string input = lz.encode(str);

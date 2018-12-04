6c05f4af700c0b30287ebd0f7fa647e2f108c40c
Lucas Cavalcanti <lhcs@cin.ufpe.br>
2018-12-04 17:25:06 -0300

    logic for commands and input and ouput

--- ipmt/src/main.cpp
+++ ipmt/src/main.cpp
@@ -1,23 +1,131 @@
 #include <iostream>
-#include "lz77.h"
+#include <string>
 
-int main () {
-    LZ77 lz(7,4);
-    std::string str = "cbacbacbcacbacbcaac";
-    // lz.set_buffer_size(512, 128);
-    // lz.set_buffer_size(511, 127);
-    // lz.set_buffer_size(255, 127);
-    // lz.set_buffer_size(256, 128);
-    // lz.set_buffer_size(65535, 255);
-    // lz.set_buffer_size(65535, 511);
-    // lz.set_buffer_size(65535, 4095);
-    // std::cout << "Encoding: " << str << std::endl;
+#include "utils.h"
 
-    std::string input = lz.encode(str);
-    // std::cout << "Encoded: " << input << std::endl;
 
-    std::string output = lz.decode(input);
-    // std::cout << "Decoded: " << output << std::endl;
+#define LS 512
+#define LL 128
+
+int main (int argc, char* argv[]) {
+   utils::argmnts arg;
+    if(argc == 1){
+        printf("No option selected.\n");
+        utils::print_help();
+        return 0;
+    }
+    if (!utils::getArguments(&arg, argc, argv))
+        return 0;
+    
+    FILE *index_file;
+    std::string compressed;
+    int bytes;
+    int size;
+    std::string code;
+    char* txt;
+    std::string index_fname;
+    LZ77 lz(LS, LL);
+    SArray SA;
+
+    switch(arg.option){
+        default:
+            printf("Invalid option selected \n");
+            utils::print_help();
+            break;
+        case 1: // Index - Compress
+            txt = utils::readFile((arg.txt_files)[0].c_str());
+            SA.init(txt);
+
+            index_fname = ((arg.txt_files)[0]).substr(0, (arg.txt_files[0]).size()-4) + ".idx";
+            index_file = fopen(index_fname.c_str(), "wb");
+            if (index_file == NULL) {
+                printf("Couldn't create Index file");
+                exit(0);
+            }
+            
+            fwrite(&(SA.n), sizeof(int), 1, index_file);
+            fwrite(&(lz.search_buffer), sizeof(int), 1, index_file);
+            fwrite(&(lz.lookahead_buffer), sizeof(int), 1, index_file);
+            
+            utils::encode(code, SA.SArr);
+            utils::encode(code, SA.Llcp);
+            utils::encode(code, SA.Rlcp);
+
+            code += txt;
+             
+            compressed = lz.encode(code);
+
+            fwrite(compressed.c_str(), sizeof(char), compressed.size(), index_file);
+            fclose(index_file);
+            break;
+
+        case 2: // Compress
+            
+            txt = utils::readFile((arg.txt_files)[0].c_str());
+
+            index_fname = ((arg.txt_files)[0]).substr(0, (arg.txt_files[0]).size()-4) + ".idx";
+            index_file = fopen(index_fname.c_str(), "wb");
+            if (index_file == NULL) {
+                printf("Couldn't create Index file");
+                exit(0);
+            }
+            
+            code += txt;
+             
+            compressed = lz.encode(code);
+
+            fwrite(compressed.c_str(), sizeof(char), compressed.size(), index_file);
+            fclose(index_file);
+            break;
+
+        case 3: // Search
+            index_fname = ((arg.txt_files)[0]);
+            
+            index_file = fopen(index_fname.c_str(), "rb");
+            if (index_file == NULL) {
+                printf("Couldn't open Index file");
+                exit(0);
+            }
+
+            fread(&(SA.n), sizeof(int), 1, index_file);
+            fread(&(lz.search_buffer), sizeof(int), 1, index_file);
+            fread(&(lz.lookahead_buffer), sizeof(int), 1, index_file);
+
+            int where = ftell(index_file);
+            fseek(index_file, 0, SEEK_END);
+            size = ftell(index_file) - where;
+            fseek(index_file, where, SEEK_SET);
+
+            char *compressed_ = new char[size];
+            
+            fread(compressed_, sizeof(char), size, index_file);
+
+            std::string sstr(compressed_, size);
+
+            code = lz.decode(sstr);            
+        
+            utils::decode(code, SA.SArr);
+            utils::decode(code, SA.Llcp);
+            utils::decode(code, SA.Rlcp);
+
+            SA.txt = new char[code.size()+1];
+            strcpy(SA.txt, code.c_str());
+            SA.txt[code.size()] = '\0';
+
+            for (std::string pat : arg.patterns) {
+                char *p = new char[pat.size() + 1];
+                strcpy(p, pat.c_str());
+                p[pat.size()] = '\0';
+                if(arg.count_only){
+                    printf("%s occurs: %d times\n", p, SA.search(p));    
+                } else {
+                    
+                }
+            }
+            fclose(index_file);
+            break;
+    }
+
 
     return 0;
 }
---
+++ ipmt/src/utils.h
@@ -0,0 +1,182 @@
+#include <getopt.h>
+#include <vector>
+#include <string.h>
+#include <fstream>
+
+
+#include "lz77.h"
+#include "SArray.h"
+
+#define DEBUG_UTILS 0
+
+
+namespace utils {   
+    struct option long_options[] = {
+        {"pattern", required_argument, NULL, 'p'},
+        {"count", no_argument, NULL, 'c'},
+        {"index", no_argument, NULL, 'i'},
+        {"compress", no_argument, NULL, 'z'},
+        {"help", no_argument, NULL, 'h'},
+        {0, 0, 0, 0}};
+
+    typedef struct argmnts
+    {
+        std::vector<std::string> patterns;
+        uint max_pattern = 0;
+        std::vector<std::string> txt_files;
+        char *pattern_file = nullptr;
+        bool count_only = false;
+        bool index_only = false;
+        bool compress_only = false;
+        int option = 0;
+    } argmnts;
+
+    void print_help()
+    {
+        printf("\n ipmt [-c] [-p FILE]|PATTERN INDEX \n");
+        printf("Search pattern in a indexed file, and INDEX should end with .idx .\n");
+        printf("\n ipmt [-z | -i] FILE \n");
+        printf("Index the FILE, and it should end with .txt .\n");
+        printf("\t--index     -i               Index and compress the FILE\n");
+        printf("\t--compress  -z               Only compress the FILE\n");
+        printf("\t--pattern   -p FILE          Search each pattern on file, one per line\n");
+        printf("\t--count     -c               Print only occurrences count\n");
+        printf("\t--help      -h               Show help\n");
+        printf("Made by: Lucas Cavalcanti and Roberto Fernandes  - {lhcs, rcf6}@cin.ufpe.br\n");
+    }
+
+    bool getArguments(argmnts *arg, int argc, char *argv[])
+    {
+        char opt;
+        while ((opt = getopt_long(argc, argv, "p:cizh", long_options, NULL)) != -1)
+        {
+            switch (opt)
+            {
+            case 'i':
+                arg->index_only = true;
+                break;
+            case 'z':
+                arg->compress_only = true;
+                break;
+            case 'p':
+                arg->pattern_file = strdup(optarg);
+                break;
+            case 'c':
+                arg->count_only = true;
+                break;
+            case 'h':
+                print_help();
+                return false;
+                break;
+            default:
+                printf("Invalid option selected \n");
+                exit(0);
+            }
+        }
+        
+        if(arg->index_only){
+            arg->option = 1;
+        } else if (arg->compress_only){
+            arg->option = 2;
+        } else {
+            if (arg->pattern_file == nullptr)
+            {
+                arg->patterns.push_back(argv[optind]);
+            }
+            else
+            {
+                std::ifstream f(arg->pattern_file);
+                std::string str;
+                while (getline(f, str))
+                {
+                    arg->patterns.push_back(str);
+                    if(str.size() > arg->max_pattern)
+                        arg->max_pattern = str.size();
+                }
+            }
+            arg->option = 3;
+        }
+
+        if(arg->option < 3){
+            arg->txt_files.push_back(argv[argc-1]);
+        }else {
+            arg->txt_files.push_back(argv[optind + (arg->pattern_file == nullptr)]);     
+        }
+    
+
+#if DEBUG_UTILS
+        printf("Pattern File: %s\n", arg->pattern_file);
+        printf("Patterns: ");
+        for (auto a : arg->patterns)
+        {
+            printf("%s, ", a.c_str());
+        }
+        printf("\nFiles: ");
+        for (auto a : arg->txt_files)
+        {
+            printf("%s, ", a.c_str());
+        }
+        printf("\n");
+        printf("Count: %d\n", arg->count_only);
+#endif
+        return true;
+    }
+
+
+
+    char* readFile(const char* f){
+        FILE *file = fopen(f, "rb");
+        if (file == NULL) {
+            printf("Couldn't create file: %s.", f);
+            exit(0);
+        }
+
+        fseek(file, 0, SEEK_END);
+        int tam = (int)ftell(file);
+        fseek(file, 0, SEEK_SET);
+
+        char *data = new char[tam + 1];
+        data[tam] = '\0';
+        fread(data, 1, tam, file);
+        fclose(file);
+        return data;
+    }
+
+    // char* writeIndex(){
+
+    //     return;
+    // }
+
+
+//    char* readIndex(){
+//         return ;
+//     }
+
+    void encode(std::string &dest, std::vector<int> &src) {
+        int n = (int)src.size();
+        for (int i = 0; i < n; ++i) {
+            dest += std::to_string(src[i]);
+            dest += "$";
+        }
+        dest += "#";
+        
+    }
+
+    void decode(std::string &src, std::vector<int> &dest) {
+        int n = (int)src.size();
+        std::string temp;
+        for (int i = 0; i < n; ++i) {
+            if(src[i] == '$'){
+                dest.push_back(stoi(temp));
+                temp.clear();
+            } else if (src[i] == '#'){
+                src = src.substr(i+1);
+                return;
+            }
+            else
+                temp += src[i];           
+        }
+    }
+}
+
+

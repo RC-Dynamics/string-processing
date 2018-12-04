0ab1f346d7382025510ffe6542e7f4c0aab553f6
Lucas Cavalcanti <lhcs@cin.ufpe.br>
2018-12-04 17:24:32 -0300

    making some variables public

--- ipmt/src/lz77.h
+++ ipmt/src/lz77.h
@@ -21,15 +21,15 @@ class LZ77
         std::string encode(std::string text);
         std::string decode(std::string code);
         void set_buffer_size(uint_fast16_t search_buffer_size, uint_fast16_t lookahead_buffer_size);
+        std::string int_encode(uint_fast16_t x, uint_fast16_t size);
+        uint_fast16_t int_decode(std::string x);
+        int search_buffer = 0, lookahead_buffer = 0;
 
       private:
         uint_fast16_t char_in_ab(char c);
         std::vector<std::vector<uint_fast16_t> > build_fsm(std::string pat);
-        std::string int_encode(uint_fast16_t x, uint_fast16_t size);
-        uint_fast16_t int_decode(std::string x);
         std::pair<uint_fast16_t, uint_fast16_t> prefix_match(std::string window, std::string pat);
 
-        uint_fast16_t search_buffer = 0, lookahead_buffer = 0;
         std::string ab;
 };
 
